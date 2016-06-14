df0d90b706154ed97455e5f0da1d4e94679a3428
gamesketches <sam.von.ehren@gmail.com>
2016-06-13 22:38:20 -0400

    Objects change as you rotate them

---
+++ Assets/TimeShift.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeShift : MonoBehaviour {
+
+	Material materialForTest;
+	// Use this for initialization
+	void Start () {
+		materialForTest = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion currentAngles = transform.rotation;
+		materialForTest.color = new Color(Mathf.Abs(currentAngles.x), Mathf.Abs(currentAngles.y), Mathf.Abs(currentAngles.z));
+	}
+}

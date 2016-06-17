8793b10d91cb6ddadece3480188ad70a375a11e3
gamesketches <sam.von.ehren@gmail.com>
2016-06-16 21:48:51 -0400

    Did some preliminary Director work, modified objecthandling for dropping objects, kind of too tired to test right now though

---
+++ Assets/Director.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Director : MonoBehaviour {
+
+	public int layer;
+	private GameObject[] allObjects;
+	// Use this for initialization
+	void Start () {
+		layer = 0;
+		allObjects = FindObjectsOfType<GameObject>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeScene(int sceneLayer) {
+		foreach(GameObject obj in allObjects){
+			if(obj.layer == layer || obj.tag == "Player") {
+				obj.SetActive(true);
+			}
+			else {
+				obj.SetActive(false);
+			}
+		}
+	}
+
+}
--- Assets/ObjectHandling.cs
+++ Assets/ObjectHandling.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class ObjectHandling : MonoBehaviour {
 
 	GameObject intersectingObject;
+	Director director;
 	int controllerIndex;
 	// Use this for initialization
 	void Start () {
+		director = gameObject.transform.parent.transform.parent.GetComponent<Director>();
 		intersectingObject = null;
 		controllerIndex = (int)gameObject.transform.parent.gameObject.GetComponent<SteamVR_TrackedObject>().index;
 	}
@@ -15,7 +17,16 @@ public class ObjectHandling : MonoBehaviour {
 	void Update () {
 		var device = SteamVR_Controller.Input(controllerIndex);
 		if(device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
-			intersectingObject.transform.parent = gameObject.transform.parent;
+			if(intersectingObject.transform.parent != null) {
+				intersectingObject.transform.parent = null;
+				intersectingObject.GetComponent<Rigidbody>().useGravity = true;
+				if(ObjectHasSceneChangeTag() && ConvertTagToInt(intersectingObject.tag) != director.layer) {
+					director.ChangeScene(ConvertTagToInt(intersectingObject.tag));
+				}
+			}
+			else {
+				intersectingObject.transform.parent = gameObject.transform.parent;
+			}
 		}
 	}
 
@@ -29,5 +40,36 @@ public class ObjectHandling : MonoBehaviour {
 		if(other.gameObject.tag == "holdable") {
 			intersectingObject = null;
 		}
+	}
+
+	bool ObjectHasSceneChangeTag() {
+		if(intersectingObject == null) {
+			return false;
+		}
+		switch(intersectingObject.tag) {
+			case "Untagged":
+			case "Respawn":
+			case "Finish":
+			case "EditorOnly":
+			case "MainCamera":
+			case "Player":
+			case "GameController":
+				return false;
+		}
+		return false;
+	}
+
+	int ConvertTagToInt(string tag) {
+		switch(tag) {
+			case "Untagged":
+			case "Respawn":
+			case "Finish":
+			case "EditorOnly":
+			case "MainCamera":
+			case "Player":
+			case "GameController":
+				return 0;
+		}
+		return 0;
 	}
 }

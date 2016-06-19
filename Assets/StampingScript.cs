173b58a5f5bbfdcb734b04a6c42ea221567c8c5c
gamesketches <sam.von.ehren@gmail.com>
2016-06-19 16:11:05 -0400

    First pass on stamp code, probably attach this script to the end of a stamping object

---
+++ Assets/StampingScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StampingScript : MonoBehaviour {
+
+	public bool stamped;
+	// Use this for initialization
+	void Start () {
+		stamped = false;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(!stamped && other.gameObject.name == "Stamp") {
+			// replace this with actual stuff
+			GetComponent<Renderer>().material.color = Color.red;
+		}
+	}
+}

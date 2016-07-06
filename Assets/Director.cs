1682dfc1d0a4540ddfadec73cc1257e0136c224b
gamesketches <sam.von.ehren@gmail.com>
2016-07-05 21:12:46 -0400

    Have a model walking forward as a director script test

--- Assets/Director.cs
+++ Assets/Director.cs
@@ -58,6 +58,7 @@ public class Director : MonoBehaviour {
 		Debug.Log(node.InnerXml);
 
 		Debug.Log(screenplay.scenes[0].beats[0]);
+		PlayScene(xmlDoc.ChildNodes[1].FirstChild.FirstChild);
 	}
 	
 	// Update is called once per frame
@@ -76,6 +77,37 @@ public class Director : MonoBehaviour {
 				Debug.Log("set " + obj.ToString() + " to false");
 			}
 		}
+	}
+
+	void PlayScene(XmlNode theScene) {
+		//create characters here
+		GameObject character = GameObject.Find("walking");
+
+		StartCoroutine(PlayBeat(theScene.FirstChild));
+
+	}
+
+	IEnumerator PlayBeat(XmlNode Beat) {
+		GameObject character = GameObject.Find("walking");
+		Animator anim = character.GetComponent<Animator>();
+		anim.SetBool("finished", false);
+		AudioSource audio = GetComponent<AudioSource>();
+		AudioClip voiceClip = Resources.Load<AudioClip>("VoiceClips/" + Beat.ChildNodes[2].InnerText);
+		audio.clip = voiceClip;
+		audio.Play();
+		anim.SetInteger("animationId", 2);
+		float t = 0;
+		Vector3 startPosition = character.transform.position;
+		Vector3 targetPosition = startPosition;
+		targetPosition.x += int.Parse(Beat.ChildNodes[3].InnerText);
+		targetPosition.z += int.Parse(Beat.ChildNodes[4].InnerText);
+		while(t <= 0) {
+			character.transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+			t += Time.deltaTime;
+			yield return null;
+		}
+		anim.SetInteger("animationId", 0);
+		anim.SetBool("finished", true);
 	}
 
 }

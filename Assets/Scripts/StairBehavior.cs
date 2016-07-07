c7ea05fa18c1aab881e1c1a0d65dd581c653002c
gamesketches <sam.von.ehren@gmail.com>
2016-07-06 22:13:21 -0400

    Think I got going up and down stairs working

--- Assets/Scripts/StairBehavior.cs
+++ Assets/Scripts/StairBehavior.cs
@@ -5,8 +5,12 @@ public class StairBehavior : MonoBehaviour {
 
 	private GameObject[] sceneObjects;
 	private float targetY;
+	private bool entered;
+	static float deltaY;
 	// Use this for initialization
 	void Start () {
+		entered = false;
+		deltaY = 0;
 		var goList = new System.Collections.Generic.List<GameObject>();
 		GameObject[] allObjects = FindObjectsOfType<GameObject>();
 		foreach(GameObject obj in allObjects){
@@ -16,16 +20,35 @@ public class StairBehavior : MonoBehaviour {
 		}
 		sceneObjects = goList.ToArray();
 
-		targetY = (gameObject.transform.GetComponent<BoxCollider>().size.y / 2) + transform.position.y;
+		targetY = gameObject.transform.GetComponent<BoxCollider>().center.y + transform.position.y;//.size.y / 2) + transform.position.y;
 	}
 	
 	void OnTriggerEnter(Collider other) {
-		if(other.gameObject.tag == "Player") {
-			foreach(GameObject obj in sceneObjects) {
+		if(other.gameObject.tag == "Player" && !entered) {
+			entered = true;
+			float dif = Mathf.Abs(targetY - deltaY);
+			if(targetY > deltaY) {
+				deltaY += dif;
+				foreach(GameObject obj in sceneObjects) {
 				Vector3 temp = obj.transform.position;
-				temp.y = temp.y - targetY;
+				temp.y = temp.y - dif;
 				obj.transform.position = temp;
 			}
+			}
+			else if(targetY < deltaY) {
+				deltaY -= dif;
+				foreach(GameObject obj in sceneObjects) {
+				Vector3 temp = obj.transform.position;
+				temp.y = temp.y + dif;
+				obj.transform.position = temp;
+			}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			entered = false;
 		}
 	}
 }

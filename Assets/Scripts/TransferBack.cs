d41b7c5a252ab6db4155427ef84b777ba1aa01ae
gamesketches <sam.von.ehren@gmail.com>
2016-07-14 00:23:46 -0400

    Set up theater scene for VR and transfering back to main scene

---
+++ Assets/Scripts/TransferBack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TransferBack : MonoBehaviour {
+
+
+	int controllerIndex;
+	public string sceneName;
+	// Use this for initialization
+	void Start () {
+		controllerIndex = (int)gameObject.transform.parent.gameObject.GetComponent<SteamVR_TrackedObject>().index;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var device = SteamVR_Controller.Input(controllerIndex);
+		if(device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			SceneManager.LoadScene(sceneName);
+			}
+	}
+}

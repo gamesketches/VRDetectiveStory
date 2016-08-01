a93a26e04bcb237b982410de32556eb2219ea613
gamesketches <sam.von.ehren@gmail.com>
2016-07-31 20:10:43 -0400

    added outside scene audio

--- Assets/Scripts/OutsideSceneDirector.cs
+++ Assets/Scripts/OutsideSceneDirector.cs
@@ -7,6 +7,7 @@ public class OutsideSceneDirector : MonoBehaviour {
 	Animator anna;
 	Animator manager;
 	Animator stalker;
+	public AudioSource managerAudio;
 	AudioSource audio;
 	int beatNumber;
 
@@ -28,7 +29,6 @@ public class OutsideSceneDirector : MonoBehaviour {
 		beats.Add(Beat5);
 		switchingBeat = true;
 		audio = GetComponent<AudioSource>();
-		Debug.Log(audio);
 	}
 	
 	// Update is called once per frame
@@ -40,10 +40,10 @@ public class OutsideSceneDirector : MonoBehaviour {
 		}
 	}
 
-	IEnumerator PlayDialogue(string path) {
-		audio.clip = Resources.Load<AudioClip>(path);
-		audio.Play();
-		while(audio.isPlaying) {
+	IEnumerator PlayDialogue(AudioSource source, string path) {
+		source.clip = Resources.Load<AudioClip>(path);
+		source.Play();
+		while(source.isPlaying) {
 		yield return null;
 		}
 
@@ -52,12 +52,12 @@ public class OutsideSceneDirector : MonoBehaviour {
 	// Anna and Manager Talk
 	// Play Audio of their conversation
 	IEnumerator Beat1(){
-		yield return StartCoroutine(PlayDialogue("outside/Sound/manager1"));
-		yield return StartCoroutine(PlayDialogue("outside/Sound/ring"));
+		yield return StartCoroutine(PlayDialogue(managerAudio, "outside/Sound/manager1"));
+		yield return StartCoroutine(PlayDialogue(managerAudio, "outside/Sound/ring"));
 		manager.SetInteger("animationId", 1);
-		yield return StartCoroutine(PlayDialogue("outside/Sound/manager2"));
-		audio.clip = Resources.Load<AudioClip>("outside/Sound/manager3");
-		audio.Play();
+		yield return StartCoroutine(PlayDialogue(managerAudio, "outside/Sound/manager2"));
+		managerAudio.clip = Resources.Load<AudioClip>("outside/Sound/manager3");
+		managerAudio.Play();
 		Debug.Log(anna.GetCurrentAnimatorStateInfo(0).length);
 		//yield return new WaitForSeconds(anna.GetCurrentAnimatorStateInfo(0).length);
 		beatNumber++;
@@ -118,6 +118,7 @@ public class OutsideSceneDirector : MonoBehaviour {
 		stalker.SetInteger("animationId", 1);
 		Vector3 startPosition = stalker.gameObject.transform.position;
 		Vector3 endPosition = new Vector3 (-25.1f, 0.6f, -15.42f);
+		yield return StartCoroutine(PlayDialogue(audio, "outside/Sound/spirit8"));
 		while(t < 1) {
 			stalker.gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, t);
 			t += (Time.deltaTime / 3);
@@ -125,5 +126,8 @@ public class OutsideSceneDirector : MonoBehaviour {
 		}
 
 		stalker.SetInteger("animationId", 2);
+		audio.clip = Resources.Load<AudioClip>("outside/Sound/spirit9");
+		audio.Play();
+		Debug.Log("spirit9");
 	}
 }

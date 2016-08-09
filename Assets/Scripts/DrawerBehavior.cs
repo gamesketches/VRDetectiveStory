add93b72a123cc69d7ae7e216db0060ded451b3d
gamesketches <sam.von.ehren@gmail.com>
2016-08-08 22:46:43 -0400

    First pass on drawer script

---
+++ Assets/Scripts/DrawerBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawerBehavior : MonoBehaviour {
+
+	Vector3 startPosition;
+	Vector3 lastFramePosition;
+	public float maxZChange;
+	// Use this for initialization
+	void Start () {
+		startPosition = gameObject.transform.position;
+		lastFramePosition = startPosition;
+	}
+
+	void LateUpate() {
+		Vector3 temp = startPosition;
+		if(Mathf.Abs(gameObject.transform.position.z - startPosition.z) < maxZChange) {
+			temp.z = gameObject.transform.position.z;
+			gameObject.transform.position = temp;
+			lastFramePosition = temp;
+		}
+		else {
+			gameObject.transform.position = lastFramePosition;
+		}
+	}
+}
